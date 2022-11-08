46c287986c5ff448766fd69a19bc096e5dc3c376
ioya666 <malthe02dk@gmail.com>
2022-11-08 19:10:01 +0100

    yes

--- Stats/Stats.gd
+++ Stats/Stats.gd
@@ -2,20 +2,38 @@ extends Node
 
 export(int) var max_health = 1 setget set_max_health
 var health = max_health setget set_health
+export(int) var max_stamina = 1 setget set_max_stamina
+var stamina = max_stamina setget set_stamina
 
 signal no_health
 signal health_changed(value)
 signal max_health_changed(value)
-
-func set_max_health(value):
-	max_health = value
-	self.health = min(health, max_health)
-	emit_signal("max_health_changed", max_health)
+signal stamina_changed(value)
+signal max_stamina_changed(value)
 
 func set_health(value):
 	health = value
 	emit_signal("health_changed", health)
 	if health <= 0:
 		emit_signal("no_health")
+
+func set_max_health(value):
+	max_health = value
+	self.health = min(health, max_health)
+	emit_signal("max_health_changed", max_health)
+
+func set_stamina(value):
+	stamina = value
+	emit_signal("stamina_changed", stamina)
+	if stamina <= 0:
+		emit_signal("no_stamina")
+		
+func set_max_stamina(value):
+	max_stamina = value
+	self.stamina = min(stamina, max_stamina)
+	emit_signal("max_stamina_changed", max_stamina)
+
+
 func _ready():
 	self.health = max_health
+	self.stamina = max_stamina
--- UI/HealthUI.gd
+++ UI/HealthUI.gd
@@ -2,25 +2,34 @@ extends Control
 
 var health = 4 setget set_health
 var max_health = 4 setget set_max_health
-var heartspace = 15
+var stamina = 4 setget set_stamina
+var max_stamina = 4 setget set_max_stamina
 
-onready var healthUIFull = $HealthUIFull
-onready var healthUIEmpty = $HealthUIEmpty
+onready var healthBar = $HealthBar
+onready var staminaBar = $StaminaBar
 
 func set_health(value):
 	health = clamp(value, 0, max_health)
-	if healthUIFull != null:
-		healthUIFull.rect_size.x = health * heartspace
-
+	if healthBar != null:
+		pass
 func set_max_health(value):
 	max_health = max(value, 1)
 	self.health = min(health, max_health)
-	if healthUIEmpty != null:
-		healthUIEmpty.rect_size.x = max_health * heartspace
 
+func set_stamina(value):
+	stamina = clamp(value, 0, max_health)
+	max_stamina = max(value, 1)
+	if staminaBar != null:
+		pass
+
+func set_max_stamina(value):
+	max_stamina = max(value, 1)
+	self.stamina = min(stamina, max_stamina)
 func _ready():
 	self.max_health = PlayerStats.max_health
 	self.health = PlayerStats.health
+	self.max_stamina = PlayerStats.max_stamina
+	self.stamina = PlayerStats.stamina
 # warning-ignore:return_value_discarded
 	PlayerStats.connect("health_changed", self, "set_health")
 # warning-ignore:return_value_discarded
